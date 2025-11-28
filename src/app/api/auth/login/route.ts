10dfa60727dd5cf094da6580dd2ecb2feff29c64
Leinhoj <161433786+Danrev29@users.noreply.github.com>
2025-11-28 12:35:44 +0800

    Update route.ts

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { adminAuth, adminDb } from '@/lib/firebaseAdmin';
+import { adminAuth, adminDB } from '@/lib/firebaseAdmin';
 
 export async function POST(request: NextRequest) {
   try {
